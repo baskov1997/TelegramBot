3032c2210b5355fb77ffa126defdd3e9f6f7f731
Baskov Vlad <baskov.1997@mail.ru>
2023-12-20 20:50:54 +0300

    init2

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
-import random
+import asyncio
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
+
+bot = Bot("6886603537:AAHSYWTw-tY_Qn-d0xZWTL_SWtXe8glqgn0")
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(f"Hello, {message.from_user.first_name}")
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
